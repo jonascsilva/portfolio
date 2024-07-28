d1061df7db71983b85f523057d46e0cf99c42c54
jonascsilva <55038425+jonascsilva@users.noreply.github.com>
2024-07-27 23:24:10 -0300

    add vitest

--- tests/page.test.tsx
+++ tests/page.test.tsx
@@ -1,37 +1,41 @@
+import { Mock, afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+
 import { ThemeButton } from '@/_/ThemeButton'
 import { useIsMounted } from '@/_/ThemeButton/useIsMounted'
 import { getNextTheme } from '@/_/themes.utils'
 import { useTheme } from '@/_/useTheme'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
 
-jest.mock('@/_/useTheme', () => ({
-  useTheme: jest.fn()
+vi.mock('@/_/useTheme', () => ({
+  useTheme: vi.fn()
 }))
 
-jest.mock('@/_/ThemeButton/useIsMounted', () => ({
-  useIsMounted: jest.fn()
+vi.mock('@/_/ThemeButton/useIsMounted', () => ({
+  useIsMounted: vi.fn()
 }))
 
-jest.mock('@/_/themes.utils', () => ({
-  getNextTheme: jest.fn()
+vi.mock('@/_/themes.utils', () => ({
+  getNextTheme: vi.fn()
 }))
 
-jest.mock('react-icons/md', () => ({
+vi.mock('react-icons/md', () => ({
   MdDarkMode: () => <div data-testid='MdDarkMode'>DarkModeIcon</div>,
   MdLightMode: () => <div data-testid='MdLightMode'>LightModeIcon</div>
 }))
 
-const mockUseTheme = useTheme as jest.Mock
-const mockUseIsMounted = useIsMounted as jest.Mock
-const mockGetNextTheme = getNextTheme as jest.Mock
+const mockUseTheme = useTheme as Mock
+const mockUseIsMounted = useIsMounted as Mock
+const mockGetNextTheme = getNextTheme as Mock
 
 describe('ThemeButton Component', () => {
   beforeEach(() => {
-    jest.clearAllMocks()
+    vi.clearAllMocks()
   })
 
+  afterEach(cleanup)
+
   const setup = (currentTheme: 'dark' | 'light', isMounted: boolean) => {
-    const setThemeMock = jest.fn()
+    const setThemeMock = vi.fn()
 
     mockUseTheme.mockReturnValue({ currentTheme, setTheme: setThemeMock })
     mockUseIsMounted.mockReturnValue(isMounted)
@@ -44,31 +48,31 @@ describe('ThemeButton Component', () => {
   it('renders with light mode icon when current theme is dark', () => {
     setup('dark', true)
 
-    expect(screen.getByTestId('MdLightMode')).toBeInTheDocument()
-    expect(screen.queryByTestId('MdDarkMode')).not.toBeInTheDocument()
+    expect(screen.getByTestId('MdLightMode')).to.exist
+    expect(screen.queryByTestId('MdDarkMode')).to.not.exist
   })
 
   it('renders with dark mode icon when current theme is light', () => {
     setup('light', true)
 
-    expect(screen.getByTestId('MdDarkMode')).toBeInTheDocument()
-    expect(screen.queryByTestId('MdLightMode')).not.toBeInTheDocument()
+    expect(screen.getByTestId('MdDarkMode')).to.exist
+    expect(screen.queryByTestId('MdLightMode')).to.not.exist
   })
 
   it('disables the button when the component is not mounted', () => {
     setup('dark', false)
 
-    const button = screen.getByRole('button', { name: 'Change theme' })
+    const button = screen.getByRole('button', { name: 'Change theme' }) as HTMLButtonElement
 
-    expect(button).toBeDisabled()
+    expect(button.disabled).to.be.true
   })
 
   it('enables the button when the component is mounted', () => {
     setup('dark', true)
 
-    const button = screen.getByRole('button', { name: 'Change theme' })
+    const button = screen.getByRole('button', { name: 'Change theme' }) as HTMLButtonElement
 
-    expect(button).toBeEnabled()
+    expect(button.disabled).to.be.false
   })
 
   it('changes theme on button click', () => {
